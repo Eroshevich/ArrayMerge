0d9fbb058a3d1825b44ccd36c1b8ad35af9c37f4
Eroshevich Palina <30950652+palinom@users.noreply.github.com>
2017-11-14 18:28:39 +0300

    Create Merge.php

---
+++ Merge.php
@@ -0,0 +1,20 @@
+<?php
+
+$params = [
+    'parameters' => [
+        'my_mailer.gateways' => 'mail1']
+];
+$props = [
+    'parameters' => [
+        'my_mailer.gateways' => 'mail2',
+        'extra' => 'value'
+],
+    'extra' => 'value'];
+$result = array_merge($params, $props);
+foreach($result as $key=>$value) {
+    if ( array_key_exists($key,$params)) {
+        $result=array_intersect_key($props, $params);
+        $params[ $props[$key] ] = $value;
+    }
+}
+print_r($result);
