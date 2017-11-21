512607c1e782da20c26722324ec3662722b4f7c8
Eroshevich Palina <30950652+palinom@users.noreply.github.com>
2017-11-21 13:47:49 +0300

    Create NewOne.php

---
+++ NewOne.php
@@ -0,0 +1,33 @@
+
+<?php
+$params = [
+    'parameters' => [
+        'my_mailer.gateways' => 'mail1'
+    ],
+    'test-key' => 'test-value'
+];
+$props = [
+    'parameters' => [
+        'my_mailer.gateways' => 'mail2',
+        'extra' => 'value'
+    ],
+    'extra' => 'value'];
+    $result = [$params];
+    $iterator = new RecursiveArrayIterator($result);
+    
+    while ($iterator->valid()) {
+    if ($iterator->hasChildren()) {
+        foreach ($iterator->getChildren() as $key => $value) {
+            foreach ($props as $key => $value) {
+                if (array_key_exists($key, $props)){
+                $iterator[$key] = $props[$key];
+                }
+                if ($props){
+                $iterator[$key] = $value;
+                }
+            }
+        print_r ($iterator);
+        }
+    $iterator->next();
+    }
+}
