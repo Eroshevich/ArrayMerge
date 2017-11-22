07b3033326bbed825ec72b77e558729fe6763804
Eroshevich Palina <30950652+palinom@users.noreply.github.com>
2017-11-22 12:52:21 +0300

    Update BuildTree.php

--- BuildTree.php
+++ BuildTree.php
@@ -1,35 +1,23 @@
 <?php
 
-const GRAFIC_TREE_CHILD_SEPARATOR = '___';
 $params = [
     'parameters' => [
-        'my_mailer.gateways' => 'mail1']
+        'my_mailer.gateways' => 'mail1'],
 ];
 $props = [
     'parameters' => [
         'my_mailer.gateways' => 'mail2',
         'extra' => 'value'
-    ],
+],
     'extra' => 'value'];
 
-$result = array_merge($params, $props);
+$result = [$params,$props];
 
-function showDirectoryContents($result, $indent, $break)
-{
-    $items = scandir($result);
-    foreach ($items as $item) {
-        if ($item == '.' || $item == '..')
-            continue;
-        echo $indent.$item.$break;
-        $path = $result.DIRECTORY_SEPARATOR.$item;
-        if (is_dir($path) && is_readable($path) && !is_link($item)) {
-            showDirectoryContents($path, str_replace(GRAFIC_TREE_CHILD_SEPARATOR, '   ', $indent).'|'.GRAFIC_TREE_CHILD_SEPARATOR, $break);
-        }
-    }
+$arrayIterator = new RecursiveArrayIterator($result);
+$iterator = new RecursiveIteratorIterator($arrayIterator);
+
+foreach ($iterator as $key => $value) {
+  $depth = $iterator->getDepth();
+  print_r ("depth=$depth k=$key v=$value\n");
 }
-if (php_sapi_name()=='cli') {
-    showDirectoryContents('.', '', "\n");
-}
-else {
-    showDirectoryContents('.', '', "<br/>");
-}
+?>
